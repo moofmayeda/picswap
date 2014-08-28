ff65d050ca5d8537289111ac2dfb405d3b942caf
Austin Steward & moof mayeda <student@epicodus.com>
2014-08-28 15:39:59 -0700

    Delete tags from photos

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,4 +4,11 @@ class TagsController < ApplicationController
     @tag = @photo.tags.create(user_id: params[:tag][:user_id])
     redirect_to user_photo_path(@photo.user, @photo)
   end
+
+  def destroy
+    @photo = Photo.find(params[:photo_id])
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to user_photo_path(@photo.user, @photo)
+  end
 end
