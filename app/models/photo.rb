1ee90b0e0788dd5a0dc0d4d9f466ccbf9152a0f9
Austin Steward & moof mayeda <student@epicodus.com>
2014-08-29 09:21:56 -0700

    display number of favorites for a photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,8 @@ class Photo < ActiveRecord::Base
   has_many :favorites
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+
+  def faved
+    Favorite.where(photo_id: self.id).count
+  end
 end
