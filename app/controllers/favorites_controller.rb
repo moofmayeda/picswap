d3948fc30b772031596f210a63913ed3f9903d4e
Austin Steward & moof mayeda <student@epicodus.com>
2014-08-29 11:33:05 -0700

    add toggle to mark favorite button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,4 +4,11 @@ class FavoritesController < ApplicationController
     current_user.favorites.create(photo_id: @photo.id)
     redirect_to user_photo_path(@photo.user, @photo)
   end
+
+  def destroy
+    @photo = Photo.find(params[:photo_id])
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
+    redirect_to user_photo_path(@photo.user, @photo)
+  end
 end
